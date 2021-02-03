bfbd5a8e0545045d1a1942ed3b9b43a31395b153
brammont <brayanmazuera1@gmail.com>
2021-02-03 16:38:47 -0500

    adding backgroung image

--- app/src/App.js
+++ app/src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
+import { makeStyles } from '@material-ui/core/styles';
 import './App.css';
 
-function App() {
+const useStyles = makeStyles((theme) => ({
+  root:{
+    minHeight: '100vh',
+    backgroundImage: `url(${process.env.PUBLIC_URL + '/assets/bg.jpg'})`,
+
+  }
+}));
+
+export default function App() {
+  const classes = useStyles();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={classes.root}>
+
     </div>
   );
 }
-
-export default App;
